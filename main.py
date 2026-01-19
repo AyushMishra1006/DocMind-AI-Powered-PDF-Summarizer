75aa5d3fb2a39a59142860b3568d7f34a2a4f937
Ayush Mishra <ayushmishr1006@gmail.com>
2026-01-19 22:48:51 +0530

    cc

--- main.py
+++ main.py
@@ -352,6 +352,19 @@ if st.session_state.suggested_questions:
     st.markdown("</div>", unsafe_allow_html=True)
 
 
+# -------------------------------------------------
+# CHAT RENDER
+# -------------------------------------------------
+st.markdown('<div class="chat-container">', unsafe_allow_html=True)
+
+if document_text:
+    for role, msg in st.session_state.chat_history:
+        css = "user-msg" if role == "user" else "bot-msg"
+        st.markdown(f'<div class="{css}">{msg}</div>', unsafe_allow_html=True)
+else:
+    st.info("📄 Upload a document to get started")
+
+st.markdown("</div>", unsafe_allow_html=True)
 
 
 
